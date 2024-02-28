7ffa765fed55cd1a9035ce62f6d1b786ac3f4ace
Sagar Kumar <Kumarsagar500@gmail.com>
2024-02-28 14:46:31 +0530

    Dom P-1

---
+++ DOM Project 1/style.js
@@ -0,0 +1,10 @@
+const box1=document.getElementById("box1");
+box1.style.backgroundColor="yellow";
+box1.style.height="200px";
+box1.style.width="300px";
+box1.innerText="Hello, I'm a div !";
+box1.style.color="blue";
+box1.style.margin="20px";
+box1.style.padding="10px";
+box1.style.fontSize="18px";
+box1.style.fontWeight="bold";
