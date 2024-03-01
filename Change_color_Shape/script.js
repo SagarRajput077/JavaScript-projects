669576fcd92df07a26a1abda54ae2b3b948376f4
Sagar Kumar <Kumarsagar500@gmail.com>
2024-03-01 17:38:37 +0530

    updated shape and color

--- Change_color_Shape/script.js
+++ Change_color_Shape/script.js
@@ -1,30 +1,27 @@
-
-var isTriangle = false;
 var chShape = document.getElementById("change-shape");
 var colors = document.getElementById("change-color");
-chShape.addEventListener("click",changeShape);
+var circle = document.getElementById("circle");
 
+chShape.addEventListener("click", changeShape);
 
-let color = (a,b,c)=>`rgb(${a},${b},${c})`;
+colors.addEventListener('click', function() {
+    var randomR = Math.floor(Math.random() * 256);
+    var randomG = Math.floor(Math.random() * 256);
+    var randomB = Math.floor(Math.random() * 256);
 
-colors.addEventListener('click',function(){
-    let randomR = Math.floor(Math.random()*256);
-    let randomG = Math.floor(Math.random()*256);
-    let randomB = Math.floor(Math.random()*256);
-    circle.style.backgroundColor = color(randomR,randomG,randomB);
-})
-function changeShape()
-{
-  if(!isTriangle)
-  {
-    var i = document.getElementsByClassName("inner")[0];
-    i.className = "triangle-bottomleft";
-    isTriangle = true;
-  }
-  else
-  {
-    var i = document.getElementsByClassName("triangle-bottomleft")[0];
-    i.className = "inner";
-    isTriangle = false;
-  }
+    circle.style.backgroundColor = color(randomR, randomG, randomB);
+});
+
+function changeShape() {
+    var inner = document.querySelector('.inner');
+    inner.classList.remove("Ssquare", "Scircle", "Shexagon", "Srectangle");
+
+    var shapes = ["Ssquare", "Scircle", "Shexagon", "Srectangle"];
+    var randomShape = shapes[Math.floor(Math.random() * shapes.length)];
+
+    inner.classList.add(randomShape);
+}
+
+function color(a, b, c) {
+    return `rgb(${a},${b},${c})`;
 }
